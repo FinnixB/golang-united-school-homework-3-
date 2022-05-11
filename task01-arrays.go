3d3be730681a3f6bf64fb6b172fca3858bf092de
Pavel Demkovich <finn@finnix.servebeer.com>
2022-05-11 23:13:18 +0300

    final solution

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,6 +1,12 @@
 package homework
 
 func average(input [15]float32) (result float32) {
-	//Place your code here
+	result = 0.0
+	var ilen = (float32)(len(input))
+
+	for i := 0; i < len(input); i++ {
+		result = input[i]/ilen + result
+	}
+
 	return
 }
--- task02-slice.go
+++ task02-slice.go
@@ -1,6 +1,13 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
-	//Place your code here
+	result = make([]int64, len(input))
+
+	var ilen = len(input)
+
+	for i := 0; i < ilen; i++ {
+		result[ilen-i-1] = input[i]
+	}
+
 	return
 }

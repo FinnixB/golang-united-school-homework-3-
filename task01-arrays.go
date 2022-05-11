f2e8df85aeed6d096bae3dd6d22c13ba03c4803b
Pavel Demkovich <finn@finnix.servebeer.com>
2022-05-11 23:14:29 +0300

    final solution - float fix

--- task01-arrays.go
+++ task01-arrays.go
@@ -2,11 +2,12 @@ package homework
 
 func average(input [15]float32) (result float32) {
 	result = 0.0
-	var ilen = (float32)(len(input))
 
 	for i := 0; i < len(input); i++ {
-		result = input[i]/ilen + result
+		result = input[i] + result
 	}
+
+	result = result / len(input)
 
 	return
 }
